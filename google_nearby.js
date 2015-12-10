944de6c05cd5ba4e41fb855aa26262a68acfb857
Gjorge Karakabakov <gorgekara@gmail.com>
2015-12-10 11:28:46 +0100

    Initial commit

---
+++ google_nearby.js
@@ -0,0 +1,15 @@
+nearby = {
+	setKey: function (key) {
+		this.API = key;
+	},
+	getPlaces: function (options, callback) {
+		var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/' + options.output;
+		url += '?location=' + options.lat + ',' + options.lng;
+		url += '&radius=' + (options.radius || 500);
+		url += '&types=' + options.types;
+		url += '&key=' + this.API
+		url += '&sensor=' + (options.sensor || false)
+
+		HTTP.call('GET', url, { timeout: 5000 }, callback);
+	}
+};
